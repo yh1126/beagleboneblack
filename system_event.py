ed88fde8cfb5e032cb1ef7778b54d739ca56b958
yh1126 <cake_sk_yh@icloud.com>
2016-09-27 18:00:52 +0900

    fix typos in system_event

--- system_event.py
+++ system_event.py
@@ -4,56 +4,64 @@
 
 import pysftp
 import time
-import Rpig.GPIO as GPIO
+import RPi.GPIO as GPIO
 import os
 import datetime
 import event
 
 class Manager:
 
+
+
   def __init__(self):
-    INTAVAL = 3
-    SLLEPTIME = 2
-    SENSOR_PIN = 18
-    HOST = '172.21.42.152'
-    PORT = '22'
-    USER = 'iwataalb'
-    PRIVATE_KEY_FILE = '/home/pi/.ssh/id_rsa'
-    name = ''
-    uplploadPath = '/home/iwatalab/upload_image'
+    self.INTAVAL = 3
+    self.SLEEPTIME = 2
+    self.SENSOR_PIN = 18
+    self.HOST = '172.21.42.152'
+    self.PORT = 22
+    self.USER = 'iwatalab'
+    self.PRIVATE_KEY_FILE = '/home/pi/.ssh/id_rsa'
+    self.name = ''
+    self.uploadPath = '/home/iwatalab/upload_image'
     self.evt = event.Event()
+    self.todaydetail = datetime.datetime.today()
 
-  def exec(self):
+  def execute(self):
     GPIO.setmode(GPIO.BCM)
-    GPIT.setup(SENSOR_PIN, GPIO.IN)
-    st = time.time()-INTABAL
+    GPIO.setup(self.SENSOR_PIN, GPIO.IN)
+    self.st = time.time()-self.INTAVAL
     while True:
-      if(GPIO.input(SENSOR_PIN)==GPIO.HIGH) and (st +INTAVAL < time.time()):
-        st = time.time()
-        print("待機中")
+      print("待機中")
+      if(GPIO.input(self.SENSOR_PIN)==GPIO.HIGH) and (self.st + self.INTAVAL < time.time()):
+        print ("人を感知しました")
+        self.st = time.time()
         self.evt(self)
-      time.sleep(SLEEPTIME)
+      time.sleep(self.SLEEPTIME)
     GPIO.cleanup()
-  
 
-def handle_upload(fName):
-  fPath = '/home/pisurcamera/' + fName + '.jpg'
-  sftp = pysftp.Connection(HOST, username=USER, port=PORT, privat_key=PRIVATE_KEY_FILE)
+
+def handle_shoot(self, earg):
+  self.todaydetail = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
+  print self.todaydetail
+  os.system('raspistill -o ' + self.todaydetail + '.jpg -vf -hf -w 1024 -h 768 -t 1000 -ex antishake')
+  time.sleep(2)
+
+def handle_upload(self, earg):
+  self.fPath = '/home/pi/surcamera/' + self.todaydetail + '.jpg'
+  sftp = pysftp.Connection(self.HOST, username=self.USER, port=self.PORT, private_key=self.PRIVATE_KEY_FILE)
   sftp.listdir()
-  sftp.chdir(uploadPath)
+  sftp.chdir(self.uploadPath)
   sftp.getcwd()
-  sftp.put(fPath)
+  sftp.put(self.fPath)
+  for item in sftp.execute('ls -l /home/iwatalab/upload_image'):
+    print item,     #結果を表示
+
   sftp.close()
 
-def handle_shoot(self):
-  todaydetail = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
-  print todaydetail
-  os.system('raspistill -o' + todaydetail + '.jpg -vf -hf -w 1024 -h 768 -t 1000 -ex antishake')
-  time.sleep(2)
- 
-if __name__ == __'__main__':
-  man  = Maneer()
+if __name__ == '__main__':
+  man  = Manager()
   man.evt += handle_shoot
   man.evt += handle_upload
-  man.exec()
+  man.execute()
+
 
